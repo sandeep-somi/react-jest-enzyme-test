95e05672800f2bbfefea8405fc32d2b2074006ae
Sandeep Rajput <soami.ssr@gmail.com>
2019-05-03 21:10:35 +0530

    updated base url to remote from local

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-// export const API_URL = 'https://andy-apis.herokuapp.com';
-export const API_URL = 'http://localhost:8080';
+export const API_URL = 'https://andy-apis.herokuapp.com';
+// export const API_URL = 'http://localhost:8080';
 
 export const initialUser = {
   email: '',
